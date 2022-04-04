275b3d31afff4fb34b9c49c869f0858a04766e35
soyludev <96364862+soyludev@users.noreply.github.com>
2022-04-04 22:49:19 +0200

    Update basicStorage.sol

--- basicStorage.sol
+++ basicStorage.sol
@@ -1,1 +1,17 @@
+pragma solidity >=0.4.16 <0.9.0;
 
+contract SimpleStorage{
+    //local and global vars !!!
+
+    uint storedData;      //not public
+    //uint public storedData;   //public
+
+    function set(uint x) public {    // you can call func outside
+        storedData = x; 
+
+    }
+
+    function get() public view returns (uint){   // no modification
+        return storedData;
+    }
+}
