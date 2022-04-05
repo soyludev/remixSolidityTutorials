f2a7106d5230c2accb295d5f3f228642363beb3b
soyludev <96364862+soyludev@users.noreply.github.com>
2022-04-05 23:02:41 +0200

    Create automatingTransfers.sol

---
+++ automatingTransfers.sol
@@ -0,0 +1,73 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+// time stamp of the block
+
+contract myGame {
+
+    uint public playerCount = 0;
+    uint public pot = 0;
+
+    address public dealer;
+    //Player[] public players;
+    Player[] public playersInGame;
+    mapping (address => Player) public players;
+    enum Level {Novice, Intermediate, Advanced}       //it must be one of these three
+    
+
+    struct Player {  //records, similiar like object in OOP
+        address playerAddress;
+        Level playerLevel; 
+        string firstName; 
+        string lastName; 
+        uint createdTime;
+
+    }
+
+    constructor(){
+        dealer = msg.sender;
+    }
+
+    function addPlayer(string memory firstName, string memory lastName) private {   //memory = how it saved
+        Player memory newPlayer = Player(msg.sender, Level.Novice,firstName, lastName, block.timestamp);
+            
+        players[msg.sender] = newPlayer;//players.push(Player(firstName, lastName));
+        playersInGame.push(newPlayer);
+    }
+
+    function getPlayerLevel(address playerAddress) private view returns(Level){
+        Player storage player = players[playerAddress];
+        return player.playerLevel;
+
+    }
+
+    function changePlayerLevel(address playerAddress) private {
+        Player storage player = players[playerAddress];
+        if (block.timestamp >= player.createdTime + 20) {
+            player.playerLevel = Level.Intermediate; 
+        }
+    }
+
+    function joinGame(string memory firstName, string memory lastName) payable public {
+        require(msg.value == 25 ether, "The joining fee is 25 ethers");
+        if(payable(dealer).send(msg.value)){
+            addPlayer(firstName, lastName);
+            playerCount += 1;
+            pot += 25; 
+        } 
+    }
+
+    function payOutWinners(address loserAddress) payable public{
+        require(msg.sender == dealer, "Only the dealer can payout the winners");
+        require(msg.value == pot*(1 ether) );
+
+        uint payoutVal = msg.value/(playerCount-1);
+        for (uint i=0; i < playersInGame.length; i++){
+            address currentPlayerAddress = playersInGame[i].playerAddress; 
+            if(currentPlayerAddress != loserAddress){
+                payable(currentPlayerAddress).transfer(payoutVal);
+            }
+
+        }
+    }
+}
+
